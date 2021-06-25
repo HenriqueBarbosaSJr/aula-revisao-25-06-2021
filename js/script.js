ed25c49bdc300c6b086f687db43d4982477b8445
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2021-06-25 10:13:05 -0300

    Exemplo de funções e uso do filter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // Referências do DOM - HTML
 
+const btnA = document.getElementById('btnA');
+const btnB = document.getElementById('btnB');
 
 
 
@@ -12,11 +14,43 @@ let obj = {
     email:'joao@gmail.com',
     idade:53
  };
+ 
+// Exemplo da função filter
+let vet2 = vet.filter(item => item > 50);
 
- // Desestruturação de dados em objetos
+console.log(vet2);
+
+// 1 tipo de função
+function nome(){
+    console.log('Teste de função');
+};
+nome();
+
+// 2 tipo de função (função anômima )
+btnA.onclick = function (){
+    console.log('Teste de função anônima');
+};
+
+// 3 tipo de função (Arrow function)
+btnB.onclick = ()=>{
+    console.log('Teste de Arrow function');
+};
+
+
+
+/*
+
+// Desestruturação de dados em objetos
 
 let {nome, email, idade } = obj;
-console.log(nome, email, idade);
+//console.log(nome, email, idade);
+
+// Template string
+console.log(`A idade = ${idade} do usuário ${nome}`);
+
+console.log('A idade = ', idade, 'do usuário', nome);
+*/
+
 
  /*
 let nome = obj.nome;
@@ -25,8 +59,6 @@ let email = obj.email;
 let idade = obj.idade;
 console.log(email, idade);
 */
-
-
 
 
 // Desestruturação de dados em vetores
@@ -44,7 +76,4 @@ numC = vet[2];
 
 console.log(numA, numB, numC);
 */
-
-
-
 
